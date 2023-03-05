60ded002ab08720ac6ba23c1f7f7cda24fd364b6
Macailh <salvadorenriquegb@gmail.com>
2023-03-05 07:10:49 -0600

    Initial commit

---
+++ src/main/java/com/salvadorgerman/bottelegramspringboot/BotTelegramSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.salvadorgerman.bottelegramspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotTelegramSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotTelegramSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salvadorgerman/bottelegramspringboot/BotTelegramSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvadorgerman.bottelegramspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotTelegramSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
